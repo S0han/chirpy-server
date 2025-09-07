311c50e13d0a747fd8193b0cba4cd1f02a9351fb
S0han <kane.pereira18@gmail.com>
2025-09-07 14:57:04 -0400

    update: add middleware to check hits to server

--- main.go
+++ main.go
@@ -1,12 +1,40 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (apiCfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		apiCfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (apiCfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf("Hits: %v", apiCfg.fileserverHits.Load())))
+}
+
+func (apiCfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
+	apiCfg.fileserverHits.Store(0)
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Hits have been reset to 0"))
+}
 
 func main() {
 	mux := http.NewServeMux()
+
+	apiCfg := &apiConfig{}
 
 	// /healthz
 	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
@@ -14,13 +42,12 @@ func main() {
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("OK"))
 	})
-
-	// fs := http.FileServer(http.Dir("."))
-	// mux.Handle("/", fs)
+	mux.Handle("/metrics", http.HandlerFunc(apiCfg.handlerMetrics))
+	mux.Handle("/reset", http.HandlerFunc(apiCfg.handlerReset))
 
 	// fileserver at /app/
 	fs := http.FileServer(http.Dir("."))
-	mux.Handle("/app/", http.StripPrefix("/app/", fs))
+	mux.Handle("/app/", http.StripPrefix("/app/", apiCfg.middlewareMetricsInc(fs)))
 
 	s := &http.Server{
 		Addr:    ":8080",
