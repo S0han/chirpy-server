d743f9cbd7121f38be12c4165ffd401d3ec39c39
S0han <kane.pereira18@gmail.com>
2025-09-14 23:19:10 -0400

    update: add createUser function and modify reset function to clear user data table

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module chirpy-server
 
 go 1.24.5
+
+require (
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/kelseyhightower/envconfig v1.4.0 // indirect
+	github.com/lib/pq v1.10.9 // indirect
+)
--- main.go
+++ main.go
@@ -1,16 +1,33 @@
 package main
 
 import (
+	"chirpy-server/internal/database"
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"strings"
 	"sync/atomic"
+	"time"
+
+	"github.com/joho/godotenv"
+
+	_ "github.com/lib/pq"
 )
 
 type apiConfig struct {
 	fileserverHits atomic.Int32
+	queries        *database.Queries
+	platform       string
+}
+
+type UserResponse struct {
+	ID        string    `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Email     string    `json:"email"`
 }
 
 func (apiCfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
@@ -35,8 +52,44 @@ func (apiCfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) 
 }
 
 func (apiCfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
+	if apiCfg.platform != "dev" {
+		respondWithError(w, 403, "403 Forbidden")
+		return
+	}
+
+	if err := apiCfg.queries.DeleteAllUsers(r.Context()); err != nil {
+		respondWithError(w, http.StatusInternalServerError, "database error")
+		return
+	}
+
 	apiCfg.fileserverHits.Store(0)
-	respondWithJSON(w, http.StatusOK, []byte("Hits have been reset to 0"))
+	respondWithJSON(w, http.StatusOK, map[string]string{"status": "ok"})
+}
+
+func (apiCfg *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Request) {
+	type createUserParams struct {
+		Email string `json:"email"`
+	}
+	var p createUserParams
+	if err := json.NewDecoder(r.Body).Decode(&p); err != nil {
+		respondWithError(w, http.StatusBadRequest, "invalid JSON")
+		return
+	}
+
+	u, err := apiCfg.queries.CreateUser(r.Context(), p.Email)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "database error")
+		return
+	}
+
+	resp := UserResponse{
+		ID:        u.ID.String(),
+		CreatedAt: u.CreatedAt,
+		UpdatedAt: u.UpdatedAt,
+		Email:     u.Email,
+	}
+
+	respondWithJSON(w, http.StatusCreated, resp)
 }
 
 func handlerValidateChirp(w http.ResponseWriter, r *http.Request) {
@@ -88,15 +141,32 @@ func respondWithError(w http.ResponseWriter, code int, msg string) {
 }
 
 func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(code)
 	json.NewEncoder(w).Encode(payload)
 }
 
 func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Println("warning: .env not loaded:", err)
+	}
+
+	dbURL := os.Getenv("DB_URL")
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	platform := os.Getenv("PLATFORM")
+
+	dbQueries := database.New(db)
+
 	mux := http.NewServeMux()
 
-	apiCfg := &apiConfig{}
+	apiCfg := &apiConfig{
+		queries:  dbQueries,
+		platform: platform,
+	}
 
 	// /healthz endpoint
 	mux.Handle("GET /api/healthz", http.HandlerFunc(handlerHealthz))
@@ -107,6 +177,8 @@ func main() {
 	mux.Handle("POST /api/validate_chirp", http.HandlerFunc(handlerValidateChirp))
 	// /reset endpoint
 	mux.Handle("POST /admin/reset", http.HandlerFunc(apiCfg.handlerReset))
+	// /create user
+	mux.Handle("POST /api/users", http.HandlerFunc(apiCfg.handlerCreateUser))
 
 	// fileserver at /app/
 	fs := http.FileServer(http.Dir("."))
